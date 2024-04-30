1905e9a20dab545bf17a3de8c880b8fd495f67fe
bryanhad <bryanhadinata@gmail.com>
2024-04-30 22:20:48 +0700

    feat: add download button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
-import Image from "next/image";
+"use client"
+
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  );
+    const handleDownloadPDF = async () => {
+        console.log('clicked!')
+        try {
+          // HTML string to be sent to the server
+          const htmlString = '<html><body><h1>Hello, chibaaaaaaai!</h1></body></html>';
+    
+          const response = await fetch('http://localhost:5000/generate-pdf', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              // Add any additional headers if needed
+            },
+            // Convert HTML string to JSON and send it in the request body
+            body: JSON.stringify({ html: htmlString }),
+            // Add any additional options if needed
+          });
+    
+          if (!response.ok) {
+            throw new Error('Failed to fetch PDF');
+          }
+    
+          const blob = await response.blob();
+          const url = URL.createObjectURL(blob);
+    
+          const link = document.createElement('a');
+          link.href = url;
+          link.download = 'generated.pdf';
+    
+          document.body.appendChild(link);
+          link.click();
+    
+          // Clean up
+          URL.revokeObjectURL(url);
+          document.body.removeChild(link);
+        } catch (error) {
+          console.error('Error downloading PDF:', error);
+          // Handle error
+        }
+      };
+
+    return (
+        <div>
+            <Button onClick={() => handleDownloadPDF()}>
+                DOWNLOAD PDF BRO
+            </Button>
+        </div>
+    )
 }
