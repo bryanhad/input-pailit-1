a96ff32a593c06aca7a5f9da96088001131fcbac
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-05-03 04:39:05 +0700

    fix: add generate pdf url as an env

--- src/app/(must-login)/dashboard/_components/table/DownloadButton.tsx
+++ src/app/(must-login)/dashboard/_components/table/DownloadButton.tsx
@@ -1,11 +1,11 @@
-"use client"
+'use client'
 
-import LoadingButton from "@/components/LoadingButton"
-import { useToast } from "@/components/ui/use-toast"
-import axios from "axios"
-import { useState } from "react"
-import { getCreditorInfo } from "./actions"
-import { cn } from "@/lib/utils"
+import LoadingButton from '@/components/LoadingButton'
+import { useToast } from '@/components/ui/use-toast'
+import axios from 'axios'
+import { useState } from 'react'
+import { getCreditorInfo } from './actions'
+import { cn } from '@/lib/utils'
 
 function DownloadButton({ id }: { id: string }) {
     const [loading, setLoading] = useState(false)
@@ -13,42 +13,40 @@ function DownloadButton({ id }: { id: string }) {
 
     const handleDownloadPDF = async () => {
         setLoading(true)
-        console.log("clicked!")
+        console.log('clicked!')
         try {
             const creditorWithAttahcments = await getCreditorInfo(id)
             if (!creditorWithAttahcments) {
                 throw new Error(`Creditor with id '${id}' not found.`)
             }
 
-            console.log(creditorWithAttahcments)
-            // const res = await axios.post(
-            //     "https://pdf-generator.bryanhadinata.com/generate-pdf",
-            //     { data: creditorWithAttahcments },
-            //     { responseType: "arraybuffer" }
-            // )
-            // if (res.status < 200 && res.status >= 300) {
-            //     throw new Error('Failed to get PDF from server.')
-            // }
-            // // Create a Blob from the Buffer
-            // const blob = new Blob([res.data], { type: "application/pdf" })
-            // // Create a temporary URL for the Blob
-            // const url = window.URL.createObjectURL(blob)
-            // const link = document.createElement("a")
-            // link.href = url
-            // link.download = `${creditorWithAttahcments.slug}.pdf`
-            // link.click()
-            // window.URL.revokeObjectURL(url)
+            const res = await axios.post(
+                process.env.NEXT_PUBLIC_PDF_GENERATOR_URL as string,
+                { data: creditorWithAttahcments },
+                { responseType: 'arraybuffer' }
+            )
+            if (res.status < 200 && res.status >= 300) {
+                throw new Error('Failed to get PDF from server.')
+            }
+            // Create a Blob from the Buffer
+            const blob = new Blob([res.data], { type: 'application/pdf' })
+            // Create a temporary URL for the Blob
+            const url = window.URL.createObjectURL(blob)
+            const link = document.createElement('a')
+            link.href = url
+            link.download = `${creditorWithAttahcments.slug}.pdf`
+            link.click()
+            window.URL.revokeObjectURL(url)
 
             toast({
-                title: "Successfully downloaded PDF.",
-                description: `Check your browser's download history`
+                title: 'Successfully downloaded PDF.',
+                description: `Check your browser's download history`,
             })
-
         } catch (err: any) {
-            console.error("Error downloading PDF:", err)
+            console.error('Error downloading PDF:', err)
             toast({
                 variant: 'destructive',
-                title: "Failed to download PDF.",
+                title: 'Failed to download PDF.',
                 description: err.message || 'Something went wrong.',
             })
         } finally {
@@ -58,9 +56,9 @@ function DownloadButton({ id }: { id: string }) {
 
     return (
         <LoadingButton
-            className={cn({'animate-pulse cursor-not-allowed': loading})}
+            className={cn({ 'animate-pulse cursor-not-allowed': loading })}
             onClick={() => handleDownloadPDF()}
-            variant={"outline"}
+            variant={'outline'}
             loading={loading}
             loadingMessage="Generating PDF.."
         >
