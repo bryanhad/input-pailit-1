62e93832de74c801a0169c79141f3ad85ce8f234
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-06-19 13:14:48 +0700

    fix: styling issue in modal overlay clashing with sidebar overlay now modal overlay is higher than sidebar overlay!

--- src/app/_components/BankruptcyVerdict.tsx
+++ src/app/_components/BankruptcyVerdict.tsx
@@ -7,7 +7,7 @@ type BankruptcyVerdictProps = {
 function BankruptcyVerdict({ verdictNumber }: BankruptcyVerdictProps) {
     return (
         <div className='space-y-4'>
-            <h2 className='font-semibold tracking-tighter'>{verdictNumber}</h2>
+            <h2 className='font-semibold tracking-tighter text-center'>{verdictNumber}</h2>
             <p className='italic font-thin'>
                 PT. Bakso Jaya Mantab adalah sebuah Perusahaan Perseroan Terbatas lingkup usaha dalam bidang Perdagangan besar, berlokasi di jalan Sukahati III, Sukasari, Tangerang - Provinsi Banten, dinyatakan dalam keadaan pailit dengan segala akibat hukumnya berdasarkan Putusan Pengadilan Niaga pada Pengadilan Negeri Jakarta Pusat Nomor : 47 / Pdt.Sus / Pailit / 2015 / PN.NIAGA.JKT.PST tanggal 03 Februari 2016.
             </p>
