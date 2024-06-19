ae8c228cac76d2b09995fd6d01825b4f430c5f1c
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-06-19 11:56:54 +0700

    feat: add go to dashboard button on the welcome page if the user is already logged in

--- src/app/_components/RootPageTitle.tsx
+++ src/app/_components/RootPageTitle.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function RootPageTitle() {
   return (
     <div className='flex flex-col max-sm:gap-2 sm:items-end text-center items-center'>
-        <h1 className='text-4xl'>PT. Bakso Jaya Mantab</h1>
+        <h1 className='text-4xl xl:text-3xl'>PT. Bakso Jaya Mantab</h1>
         <p className=''>(Dalam Pailit)</p>
     </div>
 )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image'
 import BankruptcyVerdict from './_components/BankruptcyVerdict'
 import RootPageTitle from './_components/RootPageTitle'
+import SignInButton from '@/auth/components/sign-in-button'
+import { auth } from '@/auth'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
-export default function Home() {
+export default async function Home() {
+    const session = await auth()
+
     return (
         <div className=" pt-[2vh] lg:pt-[5vh] xl:pt-[8vh] lg:flex lg:justify-center fade-in duration-500">
             <div className="flex flex-col lg:flex-row max-lg:items-center gap-4">
@@ -16,8 +22,13 @@ export default function Home() {
                     />
                     <RootPageTitle />
                 </div>
-                <div className="w-full max-w-[95%] rounded-lg shadow-sm border p-6 bg-white lg:max-w-[400px]">
+                <div className="w-full max-w-[95%] rounded-lg shadow-sm border p-6 bg-white lg:max-w-[400px] flex flex-col justify-between">
                     <BankruptcyVerdict verdictNumber="47 / Pdt.Sus / Pailit / 2015 / PN.NIAGA.JKT.PST" />
+                    {session?.user && (
+                        <Button asChild>
+                            <Link href={'/dashboard'}>Go To Dashboard</Link>
+                        </Button>
+                    )}
                 </div>
             </div>
         </div>
