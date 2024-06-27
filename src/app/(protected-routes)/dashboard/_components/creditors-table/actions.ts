955ad09e5d6d6603d6ca347dfe309fa19b01b53e
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-06-27 16:38:19 +0700

    fix: forgor to change searchFilter action to match the new creditors schema

--- src/app/(protected-routes)/dashboard/_components/creditors-table/actions.ts
+++ src/app/(protected-routes)/dashboard/_components/creditors-table/actions.ts
@@ -64,7 +64,7 @@ export async function fetchCreditors({
                       },
                   },
                   {
-                      alamatKuasaHukum: {
+                      alamat: {
                           contains: searchString,
                           mode: "insensitive",
                       },
